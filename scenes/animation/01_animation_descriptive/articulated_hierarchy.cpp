98bade2b9375c1609420d7e2967b6a6e14dae78c
Matthew Rousseau <matthew.rousseau@epita.fr>
2020-10-19 19:23:16 +0200

    Add body and wings to bird

--- scenes/animation/01_animation_descriptive/articulated_hierarchy.cpp
+++ scenes/animation/01_animation_descriptive/articulated_hierarchy.cpp
@@ -11,42 +11,58 @@ using namespace vcl;
 
 void scene_model::setup_data(std::map<std::string,GLuint>& shaders, scene_structure& , gui_structure& )
 {
-    const float radius_body = 0.25f;
+    const float radius_head = 0.20f;
+    const float radius_body = 0.5f;
     const float radius_arm = 0.05f;
     const float length_arm = 0.2f;
 
+    vec3 bird_color = {0.0f, 0.3f, 0.6f};
+
+    mesh_drawable body = mesh_drawable(mesh_primitive_sphere(radius_body, {0,0,0}, 40, 40));
+    body.uniform.transform.scaling_axis = vec3{0.6f, 0.4f, 1.0f};
+    body.uniform.transform.scaling = 0.9f;
+    body.uniform.color = bird_color;
+
     // The geometry of the body is a sphere
-    mesh_drawable body = mesh_drawable( mesh_primitive_sphere(radius_body, {0,0,0}, 40, 40));
+    mesh_drawable head = mesh_drawable(mesh_primitive_sphere(radius_head, {0,0,0}, 40, 40));
+    head.uniform.color = bird_color;
 
     // Geometry of the eyes: black spheres
-    mesh_drawable eye = mesh_drawable(mesh_primitive_sphere(0.05f, {0,0,0}, 20, 20));
+    mesh_drawable eye = mesh_drawable(mesh_primitive_sphere(0.045f, {0,0,0}, 20, 20));
     eye.uniform.color = {0,0,0};
 
-    // Shoulder part and arm are displayed as cylinder
-    mesh_drawable shoulder = mesh_primitive_cylinder(radius_arm, {0,0,0}, {-length_arm,0,0});
-    mesh_drawable arm = mesh_primitive_cylinder(radius_arm, {0,0,0}, {-length_arm/1.5f,-length_arm/1.0f,0});
+    mesh_drawable beak = mesh_drawable(mesh_primitive_cone(0.07f,{0,0.0f,0},{0,0.3f,0}));
+    beak.uniform.color = {1.0f, 0.5f, 0.2f};
 
-    // An elbow displayed as a sphere
-    mesh_drawable elbow = mesh_primitive_sphere(0.055f);
+    // Shoulder part and arm are displayed as cylinder
+    mesh_drawable wing = mesh_primitive_quad({0.0f, 0, -0.2f}, {0.0f, 0, 0.2f}, {0.5f, 0, 0.3f}, {0.5f, 0, -0.3f});
+    wing.uniform.transform.scaling = 0.9f;
+    mesh_drawable outer_wing = mesh_primitive_quad({0.0f, 0, -0.27f}, {0.0f, 0, 0.27f}, {0.5f, 0, 0.1f}, {0.5f, 0, -0.1f});
+    wing.uniform.color = bird_color;
+    outer_wing.uniform.color = bird_color;
 
     // Build the hierarchy:
     // Syntax to add element
     //   hierarchy.add(visual_element, element_name, parent_name, (opt)[translation, rotation])
     hierarchy.add(body, "body");
 
+    hierarchy.add(head, "head", "body", radius_body * vec3(0, 1/2.0f, 1/1.2f));
+
     // Eyes positions are set with respect to some ratio of the
-    hierarchy.add(eye, "eye_left", "body" , radius_body * vec3( 1/3.0f, 1/2.0f, 1/1.5f));
-    hierarchy.add(eye, "eye_right", "body", radius_body * vec3(-1/3.0f, 1/2.0f, 1/1.5f));
+    hierarchy.add(eye, "eye_left", "head" , radius_head * vec3( 1/3.0f, 1/2.0f, 1/1.5f));
+    hierarchy.add(eye, "eye_right", "head", radius_head * vec3(-1/3.0f, 1/2.0f, 1/1.5f));
+
+    const mat3 beak_rotation = rotation_from_axis_angle_mat3({1, 0, 0}, 3.14f / 1.7f);
+    // Beak position is set with respect to a ratio of the body
+    hierarchy.add(beak, "beak", "head", affine_transform(radius_head * vec3(0.0f, 0.0f, 1/2.0f), beak_rotation));
 
     // Set the left part of the body arm: shoulder-elbow-arm
-    hierarchy.add(shoulder, "shoulder_left", "body", {-radius_body+0.05f,0,0}); // extremity of the spherical body
-    hierarchy.add(elbow, "elbow_left", "shoulder_left", {-length_arm,0,0});     // place the elbow the extremity of the "shoulder cylinder"
-    hierarchy.add(arm, "arm_bottom_left", "elbow_left");                        // the arm start at the center of the elbow
+    hierarchy.add(wing, "wing_left", "body", {0.2f,0,0});
+    hierarchy.add(outer_wing, "outer_wing_left", "wing_left", {0.45f, 0, 0});
 
     // Set the right part of the body arm: similar to the left part excepted a symmetry is applied along x direction for the shoulder
-    hierarchy.add(shoulder, "shoulder_right", "body",     {{radius_body-0.05f,0,0}, {-1,0,0, 0,1,0, 0,0,1}/*Symmetry*/ } );
-    hierarchy.add(elbow, "elbow_right", "shoulder_right", {-length_arm,0,0});
-    hierarchy.add(arm, "arm_bottom_right", "elbow_right");
+    hierarchy.add(wing, "wing_right", "body",     {{-0.2f,0,0}, {-1,0,0, 0,1,0, 0,0,1}/*Symmetry*/ } );
+    hierarchy.add(outer_wing, "outer_wing_right", "wing_right",{0.45f, 0, 0});
 
 
     // Set the same shader for all the elements
@@ -76,21 +92,24 @@ void scene_model::frame_draw(std::map<std::string,GLuint>& shaders, scene_struct
     /** *************************************************************  **/
 
     // The body oscillate along the z direction
-    hierarchy["body"].transform.translation = {0,0,0.2f*(1+std::sin(2*3.14f*t))};
+    //hierarchy["body"].transform.translation = {0,0,0.2f*(1+std::sin(2*3.14f*t))};
 
-    // Rotation of the shoulder around the y axis
-    mat3 const R_shoulder = rotation_from_axis_angle_mat3({0,1,0}, std::sin(2*3.14f*(t-0.4f)) );
-    // Rotation of the arm around the y axis (delayed with respect to the shoulder)
-    mat3 const R_arm = rotation_from_axis_angle_mat3({0,1,0}, std::sin(2*3.14f*(t-0.6f)) );
+    // Rotation of the head around the x axis
+    mat3 const R_head = rotation_from_axis_angle_mat3({1,0,0}, 0.4f * std::sin(2 * 3.14f*(t-0.4f)) );
+    // Rotation of the wing around the y axis
+    mat3 const R_wing = rotation_from_axis_angle_mat3({0,0,1}, 0.7f * std::sin(2*3.14f*(t-0.6f)) );
+    mat3 const R_outer_wing = rotation_from_axis_angle_mat3({0,0,1}, 1.0f * std::sin(2*3.14f*(t-0.6f)) );
     // Symmetry in the x-direction between the left/right parts
     mat3 const Symmetry = {-1,0,0, 0,1,0, 0,0,1};
 
     // Set the rotation to the elements in the hierarchy
-    hierarchy["shoulder_left"].transform.rotation = R_shoulder;
-    hierarchy["arm_bottom_left"].transform.rotation = R_arm;
+    hierarchy["head"].transform.rotation = R_head;
 
-    hierarchy["shoulder_right"].transform.rotation = Symmetry*R_shoulder; // apply the symmetry
-    hierarchy["arm_bottom_right"].transform.rotation = R_arm; //note that the symmetry is already applied by the parent element
+    hierarchy["wing_left"].transform.rotation = R_wing;
+    hierarchy["outer_wing_left"].transform.rotation = R_outer_wing;
+
+    hierarchy["wing_right"].transform.rotation = Symmetry * R_wing; //note that the symmetry is already applied by the parent element
+    hierarchy["outer_wing_right"].transform.rotation = R_outer_wing; //note that the symmetry is already applied by the parent element
 
     hierarchy.update_local_to_global_coordinates();
 
--- scenes/current_scene.hpp
+++ scenes/current_scene.hpp
@@ -5,8 +5,8 @@
 // Help: The keywords associated to other scenes are listed below
 //
 //#define SCENE_RANDOM_FOREST // replace this keyword to change the scene
-#define SCENE_INTERPOLATION_POSITION
-// #define SCENE_ARTICULATED_HIERARCHY
+//#define SCENE_INTERPOLATION_POSITION
+#define SCENE_ARTICULATED_HIERARCHY
 // #define SCENE_SPHERE_COLLISION
 // #define SCENE_MASS_SPRING_1D
 // #define SCENE_CLOTH
